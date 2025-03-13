30ce166cb0db78aa32d5dca0e222610f9757f83e
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-03-13 23:56:47 +0900

    add input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { useState } from 'react'
 
 function App() {
+  const [input, setInput] = useState('')
+
   return (
     <>
       <div className="container">
+        <section>
+          <h1 className="text-4xl mt-10 mb-5">Input</h1>
+          <Input onChange={(e) => setInput(e.target.value)} />
+          <p className="mt-2">{input}</p>
+        </section>
+
         <section>
           <h1 className="text-4xl mt-10 mb-5">Button</h1>
           <Button variant="outline">Button</Button>

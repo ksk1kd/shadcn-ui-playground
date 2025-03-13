0ed4f2ee04f8a5f8767740fa7dfb72ff1035531e
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-03-13 23:52:57 +0900

    add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Button } from '@/components/ui/button'
+
 function App() {
   return (
     <>
-      <div className="container">Contents</div>
+      <div className="container">
+        <section>
+          <h1 className="text-4xl mt-10 mb-5">Button</h1>
+          <Button variant="outline">Button</Button>
+        </section>
+      </div>
     </>
   )
 }
